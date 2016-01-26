019b9ede437714ca1e1e025fab508b810c3d833b
studio501 <inryygy007@gmail.com>
2016-01-26 21:31:32 +0800

    reset old ver

--- primerCpp_v3.cpp
+++ primerCpp_v3.cpp
@@ -6,50 +6,9 @@
 
 using namespace std;
 
-class Object {
-public:
-	virtual ~Object(){};
-	virtual string isA(){return "a";};
-	Object(){};
-protected:
-	string _isA;
-private:
-	Object(string s)
-		:_isA(s)
-	{
-
-	}
-};
-
-class ConcreteBase{
-	
-public:
-	
-	ConcreteBase(){};
-	ConcreteBase(int a)
-	{
-		cout<<a<<endl;
-
-		_id=a;
-		cout<<_id<<endl;
-	};
-	virtual void print(ostream&){};
-	static int Object_count(){};
-protected:
-	int _id;
-	static int _object_count;
-};
-
-class c1 : public ConcreteBase{
-public:
-	c1(int val)
-	{
-		ConcreteBase::ConcreteBase(val);
-	}
-};
 int _tmain(int argc, _TCHAR* argv[])
 {
-	c1 * p = new c1(101);
+	cout<<"Hello world"<<endl;
 	system("pause");
 	return 0;
 }
