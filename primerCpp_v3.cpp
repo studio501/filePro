688c04c1d5d0df18ca7b51b3f1294de789e05406
studio501 <inryygy007@gmail.com>
2016-01-26 22:00:51 +0800

    employer data

--- primerCpp_v3.cpp
+++ primerCpp_v3.cpp
@@ -3,12 +3,28 @@
 
 #include "stdafx.h"
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
+const int Name_Limit = 20;
+struct employer
+{
+	char name[Name_Limit];
+	int gender;
+	float weight;
+	float height;
+	int age;
+};
+const char * file = "employer.data";
 int _tmain(int argc, _TCHAR* argv[])
 {
-	cout<<"Hello world"<<endl;
+	ifstream fin;
+	fin.open(file,ios_base::in | ios_base::binary);
+	if(fin.is_open())
+	{
+
+	}
+	//fin.open()
 	system("pause");
 	return 0;
 }
