96caf105b047f3405fd742d5633fbaf97fdfc9b9
studio501 <inryygy007@gmail.com>
2016-01-30 02:08:05 +0800

    ---

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// fileTest.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+
+using namespace std;
+int fileFunc(int m = 2);
+int _tmain(int argc, _TCHAR* argv[])
+{
+	fileFunc();
+	system("pause");
+	return 0;
+}
