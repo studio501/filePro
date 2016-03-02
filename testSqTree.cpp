8f67cf5978cf97e3ccb8168cf98d59fa130b87f7
studio501 <inryygy007@gmail.com>
2016-03-02 16:23:36 +0800

    ---

--- testSqTree.cpp
+++ testSqTree.cpp
@@ -10,8 +10,8 @@ typedef struct
 	int num;
 	TElemType name;
 }QElemType;
-typedef BiTree SElemType;
 
+bool deletedArr[MAX_TREE_SIZE+1];
 //打印函数
 void visit_tree(TElemType e)
 {
@@ -22,29 +22,6 @@ void visit_tree(TElemType e)
 void initBiTree(PTree &T)
 {
 	T.n=0;
-}
-
-
-
-//创建一个树结点
-void makeBiTreeNode(BiTree &T,TElemType *buf,int &i)
-{
-	if(buf[i]==EndFlag) return;
-	TElemType ch=(TElemType)buf[i];
-	if(ch==Nil) 
-	{
-		T=NULL;
-		++i;
-	}
-	else
-	{
-		T=(BiTree)malloc(sizeof(BiTNode));
-		if(!T) exit(OVERFLOW);
-		T->data=ch;
-		++i;
-		makeBiTreeNode(T->lchild,buf,i);
-		makeBiTreeNode(T->rchild,buf,i);
-	}
 }
 
 //按层次顺序输入二叉树中的结点值
@@ -74,14 +51,14 @@ void createBiTree(PTree &T,int nLine)
 			qq.name=T.nodes[i].data;
 			qq.num=i;
 			enQueue(q,qq);
-			while(s[++i]!='\0')
+			
+			while(s[++i]!='\0'&&i<MAX_TREE_SIZE)
 			{
-				while(i<MAX_TREE_SIZE&&!isQueueEmpty(q))
+				if(s[i]!=BranchEnd)
 				{
-					if(s[i]!=BranchEnd)
+					if(s[i]!=Nil)
 					{
 						++j;
-						deQueue(q,qq);
 						T.nodes[j].data=s[i];
 						T.nodes[j].parent=qq.num;
 						p.name=s[i];
@@ -89,7 +66,12 @@ void createBiTree(PTree &T,int nLine)
 						enQueue(q,p);
 					}
 				}
+				else
+				{
+					deQueue(q,qq);
+				}
 			}
+			++j;
 			if(j>MAX_TREE_SIZE) exit(OVERFLOW);
 			T.n=j;
 		}
@@ -118,239 +100,233 @@ void createBiTree(PTree &T,int nLine)
 }
 
 //判断二叉树是否为空
-bool biTreeEmpty(BiTree T)
+bool biTreeEmpty(PTree T)
 {
-	if(T) return false;
+	if(T.n) return false;
 	else return true;
 }
 
 //求树的深度
-int biTreeDepth(BiTree T)
+int biTreeDepth(PTree T)
 {
-	int i,j;
-	if(!T) return 0;
-	if(T->lchild)
-		i=biTreeDepth(T->lchild);
-	else
-		i=0;
-	if(T->rchild)
-		j=biTreeDepth(T->rchild);
-	else
-		j=0;
-	return i>j?i+1:j+1;
+	int k,m,def,max=0;
+	for(k=0;k<T.n;++k)
+	{
+		def=1;
+		m=T.nodes[k].parent;
+		while(m!=-1)
+		{
+			m=T.nodes[m].parent;
+			++def;
+		}
+		if(max<def) max=def;
+	}
+	return max;
 }
 
 //返回树的根
-TElemType root(BiTree T)
+TElemType root(PTree T)
 {
-	if(biTreeEmpty(T)) return Nil;
-	else return T->data;	
+	int i;
+	for(i=0;i<T.n;++i)
+		if(T.nodes[i].parent<0)
+			return T.nodes[i].data;
+	return Nil;
+	
 }
 
 //返回处于位置e的结点的值
-TElemType value(BiTree p)
+TElemType value(PTree T,int i)
 {
-	return p->data;
+	if(i<T.n)
+		return T.nodes[i].data;
+	else return Nil;
 }
 
 //给处于e的位置赋新值
-void assign(BiTree p,TElemType value)
+void assign(PTree &T,TElemType cur_e,TElemType value)
 {
-	p->data=value;
+	for(int j=0;j<T.n;++j)
+	{
+		if(T.nodes[j].data==cur_e)
+		{
+			T.nodes[j].data=value;
+		}
+	}
 }
 
 //若e是树的非根结点,则返回它的双亲,否则返回空
-TElemType parent(BiTree T,TElemType e)
+TElemType parent(PTree T,TElemType cur_e)
 {
+	for(int j=1;j<T.n;++j)
+		if(T.nodes[j].data==cur_e)
+			return T.nodes[T.nodes[j].parent].data;
 	return Nil;
 }
 
-//返回二叉树中指向元素值为s的结点的指针
-BiTree point(BiTree T,TElemType s)
-{
-	
-	return NULL;
-}
 
 //返回e的左孩子
-TElemType leftChild(BiTree T,TElemType e)
+TElemType leftChild(PTree T,TElemType e)
 {
 	
-	BiTree a;
-	if(T)
-	{
-		a=point(T,e);
-		if(a&&a->lchild)
-			return a->lchild->data;
-	}
-	return Nil;
-}
-
-//返回e的右孩子
-TElemType rightChild(BiTree T,TElemType e)
-{
-	
-	BiTree a;
-	if(T)
-	{
-		a=point(T,e);
-		if(a&&a->rchild)
-			return a->rchild->data;
-	}
-	return Nil;
-}
-
-//返回e的左孩子或者空
-TElemType leftSibling(BiTree T,TElemType e)
-{
-	
-	TElemType a;
-	BiTree p;
-	if(T)
-	{
-		a=parent(T,e);
-		if(a!=Nil)
-		{
-			p=point(T,a);
-			if(p->lchild&&p->rchild&&p->rchild->data==e)
-				return p->lchild->data;
-		}
-	}
+	int i,j;
+	for(i=0;i<T.n;++i)
+		if(T.nodes[i].data==e)
+			break;
+	for(j=i+1;j<T.n;++j)
+		if(T.nodes[j].parent==i)
+			return T.nodes[j].data;
 	return Nil;
 }
 
 //返回e的右兄弟或者空
-TElemType rightSibling(BiTree T,TElemType e)
+TElemType rightSibling(PTree T,TElemType e)
 {
-	TElemType a;
-	BiTree p;
-	if(T)
-	{
-		a=parent(T,e);
-		if(a!=Nil)
-		{
-			p=point(T,a);
-			if(p->lchild&&p->rchild&&p->lchild->data==e)
-				return p->rchild->data;
-		}
-	}
+	int i;
+	for(i=0;i<T.n;++i)
+		if(T.nodes[i].data==e)
+			break;
+	if(T.nodes[i+1].parent==T.nodes[i].parent)
+		return T.nodes[i+1].data;
 	return Nil;
 }
 
-//插入c为T中p结点的左或右子树
-bool insertChild(BiTree p,int LR,BiTree c)
+//打印树
+void print(PTree T)
 {
-	if(p)
+	int i;
+	cout<<"树的结点个数="<<T.n<<endl;
+	cout<<"结点      双亲\n";
+	for(i=0;i<T.n;++i)
 	{
-		if(LR==0)
+		cout<<"  "<<value(T,i);
+		if(T.nodes[i].parent>=0)
+			cout<<"        "<<value(T,T.nodes[i].parent);
+		cout<<endl;
+	}
+}
+
+//插入c为T中p结点的左或右子树
+bool insertChild(PTree &T,TElemType p,int i,PTree c)
+{
+	int j,k,l,f=1,n=0;
+	PTNode t;
+	if(!biTreeEmpty(T))
+	{
+		for(j=0;j<T.n;++j)
+			if(T.nodes[j].data==p)
+				break;
+		l=j+1;
+		if(i>1)
 		{
-			c->rchild=p->lchild;
-			p->lchild=c;
+			for(k=j+1;k<T.n;++k)
+				if(T.nodes[k].parent==j)
+				{
+					++n;
+					if(n==i-1)
+						break;
+				}
+			l=k+1;
 		}
-		else
+		if(l<T.n)
+			for(k=T.n-1;k>=l;--k)
+			{
+				T.nodes[k+c.n]=T.nodes[k];
+				if(T.nodes[k].parent>=1)
+					T.nodes[k+c.n].parent+=c.n;
+			}
+		for(k=0;k<c.n;++k)
 		{
-			c->rchild=p->rchild;
-			p->rchild=c;
+			T.nodes[l+k].data=c.nodes[k].data;
+			T.nodes[l+k].parent=c.nodes[k].parent+l;
+		}
+		T.nodes[l].parent=j;
+		T.n+=c.n;
+		while(f)
+		{
+			f=0;
+			for(j=l;j<T.n-1;++j)
+				if(T.nodes[j].parent>T.nodes[j+1].parent)
+				{
+					t=T.nodes[j];
+					T.nodes[j]=T.nodes[j+1];
+					T.nodes[j+1]=t;
+					f=1;
+					for(k=j;k<T.n;++k)
+						if(T.nodes[k].parent==j)
+							++T.nodes[k].parent;
+						else if(T.nodes[k].parent==j+1)
+							--T.nodes[k].parent;
+				}
 		}
 		return true;
 	}
-	return false;
+	else return false;
 }
 
 //根据LR为1 或0,删除T中p所指结点的左或右子树
-bool deleteChild(BiTree p,int LR)
+bool deleteChild(PTree &T,TElemType p,int i)
 {
 	
-	return false;
-}
-
-
-//先序遍历树
-void preOrderTraverse(BiTree T,visitFunc func)
-{
-	if(T)
+	int j,k,n=0;
+	LinkQueue<QElemType> q;
+	QElemType pq,qq;
+	for(j=0;j<=T.n;++j)
+		deletedArr[j]=false;
+	pq.name=Nil;
+	initQueue(q);
+	for(j=0;j<T.n;++j)
+		if(T.nodes[j].data==p)
+			break;
+	for(k=j+1;k<T.n;++k)
 	{
-		func(T->data);
-		preOrderTraverse(T->lchild,func);
-		preOrderTraverse(T->rchild,func);
+		if(T.nodes[k].parent==j)
+			++n;
+		if(n==i)
+			break;
 	}
-}
-
-//中序遍历树
-void inOrderTraverse(BiTree T,visitFunc func)
-{
-	if(T)
+	if(k<T.n)
 	{
-		inOrderTraverse(T->lchild,func);
-		func(T->data);
-		inOrderTraverse(T->rchild,func);
-	}
-}
-
-//中序遍历(非递归)
-void inOrderTraverse1(BiTree T,visitFunc func)
-{
-	stack<SElemType> S;
-	while(T||!S.empty())
-	{
-		if(T)
+		n=0;
+		pq.num=k;
+		deletedArr[k]=true;
+		++n;
+		enQueue(q,pq);
+		while(!isQueueEmpty(q))
 		{
-			S.push(T);
-			T=T->lchild;
+			deQueue(q,qq);
+			for(j=qq.num+1;j<T.n;++j)
+				if(T.nodes[j].parent==qq.num)
+				{
+					pq.num=j;
+					deletedArr[j]=true;
+					++n;
+					enQueue(q,pq);
+				}
 		}
-		else
-		{
-			T = S.top();
-			S.pop();
-			func(T->data);
-			T=T->rchild;
-		}
+		for(j=0;j<T.n;++j)
+			if(deletedArr[j]==true)
+			{
+				for(k=j+1;k<=T.n;++k)
+				{
+					deletedArr[k-1]=deletedArr[k];
+					T.nodes[k-1]=T.nodes[k];
+					if(T.nodes[k].parent>j)
+						--T.nodes[k-1].parent;
+				}
+				--j;
+			}
+		T.n-=n;
 	}
-	cout<<endl;
+	return true;
 }
 
-//中序遍历(非递归)
-void inOrderTraverse2(BiTree T,visitFunc func)
+//遍历树
+void traverseTree(PTree T,visitFunc func)
 {
-	stack<SElemType> S;
-	BiTree p;
-	S.push(T);
-	while(!S.empty())
-	{
-		p = S.top();
-		while(p)
-		{
-			S.push(p->lchild);
-			p = S.top();
-		}
-		p=S.top();
-		S.pop();
-		if(!S.empty())
-		{
-			p = S.top();
-			S.pop();
-			func(p->data);
-			S.push(p->rchild);
-		}
-	}
-	cout<<endl;
-}
-
-//后序遍历树
-void postOrderTraverse(BiTree T,visitFunc func)
-{
-	if(T)
-	{
-		inOrderTraverse(T->lchild,func);
-		inOrderTraverse(T->rchild,func);
-		func(T->data);
-	}
-}
-
-//层序遍历
-void levelOrderTraverse(BiTree T,visitFunc func)
-{
-	
+	int i;
+	for(i=0;i<T.n;++i)
+		func(T.nodes[i].data);
 	cout<<endl;
 }
 
@@ -365,6 +341,29 @@ void testSqTreeMain()
 	TElemType e1,e2;
 #if CHAR
 	createBiTree(T);
+	cout<<"tree depth is :"<<biTreeDepth(T)<<endl;
+	print(T);
+	traverseTree(T,visit_tree);
+	e1='R';
+	assign(T,e1,'V');
+	print(T);
+	traverseTree(T,visit_tree);
+	e1='D';
+	cout<<e1<<"的双亲是"<<parent(T,e1)<<"第一个孩子是"<<leftChild(T,e1)<<"下一个兄弟是"<<rightSibling(T,e1)<<endl;
+
+	createBiTree(p,2);
+	print(p);
+	traverseTree(p,visit_tree);
+	e1=root(T);
+	int cOrder = 3;
+	insertChild(T,e1,cOrder,p);
+	print(T);
+	traverseTree(T,visit_tree);
+	e1 = 'C';
+	cOrder = 1;
+	deleteChild(T,e1,cOrder);
+	print(T);
+	traverseTree(T,visit_tree);
 #else
 	createBiTree(T);
 #endif
--- testSqTree.h
+++ testSqTree.h
@@ -26,11 +26,11 @@ namespace SQT{
 
 #define MAX_TREE_SIZE 100
 
-typedef struct BiTNode
-{
-	TElemType data;
-	BiTNode *lchild,*rchild;
-}BiTNode,*BiTree;
+	typedef struct BiTNode
+	{
+		TElemType data;
+		BiTNode *lchild,*rchild;
+	}BiTNode,*BiTree;
 
 typedef struct PTNode
 {
@@ -55,68 +55,44 @@ void visit_tree(TElemType e);
 //构造空二叉树T
 void initBiTree(PTree &T);
 
-//创建一个树结点
-void makeBiTreeNode(BiTree &T,TElemType *buf,int &i);
-
 //先序创建链表二叉树
 void createBiTree(PTree &T,int nLine=1);
 
 //判断二叉树是否为空
-bool biTreeEmpty(BiTree T);
+bool biTreeEmpty(PTree T);
 
 //求树的深度
-int biTreeDepth(BiTree T);
+int biTreeDepth(PTree T);
 
 //返回树的根
-TElemType root(BiTree T);
+TElemType root(PTree T);
 
 //返回处于位置e的结点的值
-TElemType value(BiTree p);
+TElemType value(PTree p,int i);
 
 //给处于e的位置赋新值
-void assign(BiTree p,TElemType value);
+void assign(PTree &T,TElemType cur_e,TElemType value);
 
 //若e是树的非根结点,则返回它的双亲,否则返回空
-TElemType parent(BiTree T,TElemType e);
-
-//返回二叉树中指向元素值为s的结点的指针
-BiTree point(BiTree T,TElemType s);
+TElemType parent(PTree T,TElemType cur_e);
 
 //返回e的左孩子
-TElemType leftChild(BiTree T,TElemType e);
-
-//返回e的右孩子
-TElemType rightChild(BiTree T,TElemType e);
-
-//返回e的左兄弟或者空
-TElemType leftSibling(BiTree T,TElemType e);
+TElemType leftChild(PTree T,TElemType e);
 
 //返回e的右兄弟或者空
-TElemType rightSibling(BiTree T,TElemType e);
+TElemType rightSibling(PTree T,TElemType e);
+
+//打印树
+void print(PTree T);
 
 //插入c为T中p结点的左或右子树
-bool insertChild(BiTree p,int LR,BiTree c);
+bool insertChild(PTree &T,TElemType p,int i,PTree c);
 
 //根据LR为1 或0,删除T中p所指结点的左或右子树
-bool deleteChild(BiTree p,int LR);
+bool deleteChild(PTree &T,TElemType p,int i);
 
-//先序遍历树
-void preOrderTraverse(BiTree T,visitFunc func);
-
-//中序遍历树
-void inOrderTraverse(BiTree T,visitFunc func);
-
-//后序遍历树
-void postOrderTraverse(BiTree T,visitFunc func);
-
-//中序遍历(非递归)
-void inOrderTraverse1(BiTree T,visitFunc func);
-
-//中序遍历(非递归)
-void inOrderTraverse2(BiTree T,visitFunc func);
-
-//层序遍历
-void levelOrderTraverse(BiTree T,visitFunc func);
+//遍历树
+void traverseTree(PTree T,visitFunc func);
 
 };
 #endif // !__TESTSQBITREE__HH_
